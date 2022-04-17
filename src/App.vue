bebac939b54dd4ecae9a4aa04fb98d49673de34a
hanjiwon <wldnjs20725@gmail.com>
2022-04-18 02:50:33 +0900

    feat: navbar 및 main section 레이아웃

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div id="app">
     <NavigationBar/>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainSection/>
+    <HelloWorld msg="Welcome !"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import NavigationBar from './Layout/NavigationBar/NavigationBar.vue'
+import MainSection from './Layout/MainSection/MainSection.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     NavigationBar,
+    MainSection,
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/Layout/NavigationBar/NavigationBar.vue
+++ src/Layout/NavigationBar/NavigationBar.vue
@@ -1,20 +1,27 @@
 <template>
-    <div class="navbar-container">
-        <ul>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Hello</li>
-        </ul>
-    </div>
+  <div class="navbar-container">
+    <ul>
+      <li>안녕 나 NavBar야 ! </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
-    name: 'NavigationBar',
+  name: 'NavigationBar',
 
 }
 </script>
 <style lang="scss" scoped>
-    .navbar-container {
-        background-color: rgb(1, 40, 182);
+  .navbar-container {
+    background-color: rgb(26, 35, 70);
+  }
+  ul {
+    height: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    li {
+      color: white;
     }
+  }
 </style>
