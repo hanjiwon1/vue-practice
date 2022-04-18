632356720153df387402027c3f9be6a2124cfa60
hanjiwon1 <wldnjs20725@gmail.com>
2022-04-18 18:41:43 +0900

    design: 전역 css 설정

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   css: {
     loaderOptions: {
       sass: {
         additionalData: `
           @import "@/assets/scss/reset.scss";
+          @import "@/assets/scss/global.scss";
         `,
       },
     },
